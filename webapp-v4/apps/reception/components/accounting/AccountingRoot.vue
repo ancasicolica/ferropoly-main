db31cd061e306b067be493574caf1f1aa00bf8ff
Christian Kuster <christian.kuster@geoprevent.com>
2025-11-19 17:40:54 +0100

    WIP #105: Add PrimeVue tabs to AccountingRoot for team-based data display
    - Introduced tabs for rendering team-specific panels in `AccountingRoot.vue` using PrimeVue components.
    - Added computed styles for dynamic team color tags.
    - Imported and utilized `Tabs`, `TabList`, `Tab`, `TabPanels`, and `TabPanel` from PrimeVue.
    - Integrated `teamsStore` for managing team-related data.

--- webapp-v4/apps/reception/components/accounting/AccountingRoot.vue
+++ webapp-v4/apps/reception/components/accounting/AccountingRoot.vue
@@ -1,5 +1,5 @@
 <!---
-
+  The root element for the reception accounting
   Christian Kuster, CH-8342 Wernetshausen, christian@kusti.ch
   Created: 08.11.2025
 -->
@@ -7,15 +7,47 @@
 <template>
   <div>
     <call-active-warning-banner />
-    AccountingRoot.vue
+    <Tabs :value="teamsStore.teams[0].uuid" scrollable>
+      <TabList>
+        <Tab v-for="team in teamsStore.teams" :key="team.uuid" :value="team.uuid">
+          {{ team.name }}
+          <span
+              id="color-tag"
+              :style="cssVars(team.color)"
+          > &#9608;
+          </span>
+        </Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel v-for="team in teamsStore.teams" :key="team.uuid" :value="team.uuid">
+          <p class="m-0">{{ team.name }}</p>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   </div>
 </template>
 
 <script setup>
-
 import CallActiveWarningBanner from '../CallActiveWarningBanner.vue';
+
+import Tabs from 'primevue/tabs';
+import TabList from 'primevue/tablist';
+import Tab from 'primevue/tab';
+import TabPanels from 'primevue/tabpanels';
+import TabPanel from 'primevue/tabpanel';
+import {useTeamsStore} from '../../../../lib/store/TeamsStore';
+
+const teamsStore = useTeamsStore();
+
+const cssVars = function(color) {
+  return {'--team-color': color};
+};
+
 </script>
 
 <style scoped lang="scss">
+#color-tag {
+  color: var(--team-color);
+}
 
 </style>
