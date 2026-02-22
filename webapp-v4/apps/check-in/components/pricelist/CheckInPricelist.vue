3e09cd33c2f14f66e945af9821e31f6540c73308
Christian Kuster <christian@kusti.ch>
2026-02-22 14:10:27 +0100

    feat(check-in): add pricelist display with enhanced table styling
    WIP #100
    
    - Implement `CheckInPricelist` with formatted property data table
    - Introduce dynamic columns for position, location, group, and price
    - Add `FontAwesome` icons for owned properties
    - Include price formatting utility and scoped styles for property highlighting
    - Mark `PricelistList.vue` as obsolete with a warning

--- webapp-v4/apps/check-in/components/pricelist/CheckInPricelist.vue
+++ webapp-v4/apps/check-in/components/pricelist/CheckInPricelist.vue
@@ -5,13 +5,70 @@
 -->
 
 <template>
-  <div>CheckInPricelist.vue</div>
+  <div>
+    <h1>Preisliste</h1>
+    <data-table
+        :value="pricelist"
+        size="small"
+        striped-rows
+        class="mt-3"
+    >
+      <column
+          field="pricelist.position"
+          header="Pos"
+      >
+        <template #body="{data}">
+          <span>{{ data.pricelist.position + 1 }}</span>
+        </template>
+      </column>
+      <column
+          field="location.name"
+          header="Ort"
+      >
+        <template #body="{data}">
+          <span> {{ data.location.name }} </span>
+          <FontAwesomeIcon
+              v-if="data.gamedata.owner"
+              :icon="faFlag"
+              class="owned-property"
+          />
+        </template>
+      </column>
+      <column
+          field="pricelist.propertyGroup"
+          header="Gruppe"
+      />
+      <column
+          field="pricelist.price"
+          header="Kaufpreis"
+      >
+        <template #body="{data}">
+          <span>{{ formatPrice(data.pricelist.price) }}</span>
+        </template>
+      </column>
+
+    </data-table>
+  </div>
 </template>
 
 <script setup>
+import {formatPrice} from '../../../../common/lib/formatters';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import {usePropertyStore} from '../../../../lib/store/PropertyStore';
+import {computed} from 'vue';
+import {faFlag} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 
+const propertyStore = usePropertyStore();
+
+const pricelist = computed(() => {
+  return propertyStore.pricelist;
+})
 </script>
 
 <style scoped lang="scss">
-
+.owned-property {
+  color: red;
+}
 </style>
--- webapp-v4/common/components/PricelistList.vue
+++ webapp-v4/common/components/PricelistList.vue
@@ -15,49 +15,80 @@
         size="small"
         striped-rows
         class="mt-3">
-      <column field="pricelist.position" header="Pos">
+      <column
+          field="pricelist.position"
+          header="Pos"
+      >
         <template #body="{data}">
           <span>{{ data.pricelist.position + 1 }}</span>
         </template>
       </column>
-      <column field="location.name" header="Ort"></column>
-      <column field="pricelist.propertyGroup" header="Gruppe"></column>
-      <column field="pricelist.price" header="Kaufpreis">
+      <column
+          field="location.name"
+          header="Ort" />
+      <column
+          field="pricelist.propertyGroup"
+          header="Gruppe" />
+      <column
+          field="pricelist.price"
+          header="Kaufpreis"
+      >
         <template #body="{data}">
           <span>{{ formatPrice(data.pricelist.price) }}</span>
         </template>
       </column>
-      <column field="pricelist.price" header="Hauspreis">
+      <column
+          field="pricelist.price"
+          header="Hauspreis"
+      >
         <template #body="{data}">
           <span>{{ formatPrice(data.pricelist.pricePerHouse) }}</span>
         </template>
       </column>
-      <column field="pricelist.rents.noHouse" header="Miete">
+      <column
+          field="pricelist.rents.noHouse"
+          header="Miete"
+      >
         <template #body="{data}">
           <span>{{ formatPrice(data.pricelist.rents.noHouse) }}</span>
         </template>
       </column>
-      <column field="pricelist.rents.oneHouse" header="Miete 1H">
+      <column
+          field="pricelist.rents.oneHouse"
+          header="Miete 1H"
+      >
         <template #body="{data}">
           <span>{{ formatPrice(data.pricelist.rents.oneHouse) }}</span>
         </template>
       </column>
-      <column field="pricelist.rents.twoHouses" header="Miete 2H">
+      <column
+          field="pricelist.rents.twoHouses"
+          header="Miete 2H"
+      >
         <template #body="{data}">
           <span>{{ formatPrice(data.pricelist.rents.twoHouses) }}</span>
         </template>
       </column>
-      <column field="pricelist.rents.threeHouses" header="Miete 3H">
+      <column
+          field="pricelist.rents.threeHouses"
+          header="Miete 3H"
+      >
         <template #body="{data}">
           <span>{{ formatPrice(data.pricelist.rents.threeHouses) }}</span>
         </template>
       </column>
-      <column field="pricelist.rents.fourHouses" header="Miete 4H">
+      <column
+          field="pricelist.rents.fourHouses"
+          header="Miete 4H"
+      >
         <template #body="{data}">
           <span>{{ formatPrice(data.pricelist.rents.fourHouses) }}</span>
         </template>
       </column>
-      <column field="pricelist.rents.hotel" header="Miete Hotel">
+      <column
+          field="pricelist.rents.hotel"
+          header="Miete Hotel"
+      >
         <template #body="{data}">
           <span>{{ formatPrice(data.pricelist.rents.hotel) }}</span>
         </template>
@@ -71,18 +102,8 @@
 
 <script setup>
 
-import {useGameplayStore} from '../../lib/store/GamePlayStore';
-import {usePricelistStore} from '../../lib/store/pricelistStore';
-import {storeToRefs} from 'pinia';
-import {formatGameDate, formatGameTime, formatPrice} from '../lib/formatters';
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
 
-const gameplayStore  = useGameplayStore();
-const pricelistStore = usePricelistStore();
-
-const {gamename, scheduling} = storeToRefs(gameplayStore);
-const {pricelist}            = storeToRefs(pricelistStore);
+console.warn('This module is obsolete!!!');
 
 </script>
 
